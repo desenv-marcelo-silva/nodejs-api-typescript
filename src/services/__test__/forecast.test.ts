70a909b98fd2a715bd26ae91dba35dc4c4856938
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-28 20:57:04 -0300

    feat: implement tests for errors on forecast service

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,5 +1,8 @@
 import { StormGlass } from '@src/clients/stormGlass';
-import { Forecast } from '@src/services/forecast';
+import {
+  Forecast,
+  ForecastProcessingInternalError,
+} from '@src/services/forecast';
 import { Beach } from '@src/services/Beach';
 import { BeachPosition } from '@src/services/BeachPosition';
 
@@ -8,10 +11,12 @@ import stormGlassNormalizedResponseWeather3HoursFixture from '@test/fixtures/sto
 jest.mock('@src/clients/stormGlass');
 
 describe('Forecast service', () => {
+  const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
+
   it('should return the forecast for a list of beaches', async () => {
-    StormGlass.prototype.fetchPoints = jest
-      .fn()
-      .mockResolvedValue(stormGlassNormalizedResponseWeather3HoursFixture);
+    mockedStormGlassService.fetchPoints.mockResolvedValue(
+      stormGlassNormalizedResponseWeather3HoursFixture
+    );
 
     const beaches: Beach[] = [
       {
@@ -86,8 +91,35 @@ describe('Forecast service', () => {
       },
     ];
 
-    const forecast = new Forecast(new StormGlass());
+    const forecast = new Forecast(mockedStormGlassService);
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(expectedResponse);
+  });
+
+  it('should return an empty list when the beaches array is empty', async () => {
+    const forecast = new Forecast();
+    const response = await forecast.processForecastForBeaches([]);
+    expect(response).toEqual([]);
+  });
+
+  it('should throw internal processing error when something goes wrong during the rating process', async () => {
+    const beaches: Beach[] = [
+      {
+        name: 'Manly',
+        position: BeachPosition.E,
+        lat: -33.792726,
+        lng: 151.289824,
+        user: 'some-user-id',
+      },
+    ];
+
+    mockedStormGlassService.fetchPoints.mockRejectedValue(
+      'Error fetching data'
+    );
+
+    const forecast = new Forecast(mockedStormGlassService);
+    await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrow(
+      ForecastProcessingInternalError
+    );
   });
 });
--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,7 +1,15 @@
+import { ForecastPoint } from './../clients/ForecastPoint';
+import { InternalError } from './../util/errors/internal-error';
 import { StormGlass } from '@src/clients/stormGlass';
 import { Beach } from './Beach';
 import { BeachForecast } from './BeachForecast';
 import { TimeForecast } from './TimeForecast';
+
+export class ForecastProcessingInternalError extends InternalError {
+  constructor(message: string) {
+    super(`Unexpected error during the forecast processing: ${message}`);
+  }
+}
 
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
@@ -10,23 +18,33 @@ export class Forecast {
     beaches: Beach[]
   ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
-    for (const beach of beaches) {
-      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-      const enrichedBeachData = points.map((point) => ({
-        ...{
-          lat: beach.lat,
-          lng: beach.lng,
-          name: beach.name,
-          position: beach.position,
-          rating: 1,
-        },
-        ...point,
-      }));
+    try {
+      for (const beach of beaches) {
+        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+        const enrichedBeachData = this.enrichedBeachData(points, beach);
 
-      pointsWithCorrectSources.push(...enrichedBeachData);
+        pointsWithCorrectSources.push(...enrichedBeachData);
+      }
+      return this.mapForecastByTime(pointsWithCorrectSources);
+    } catch (err) {
+      throw new ForecastProcessingInternalError(err.message);
     }
+  }
 
-    return this.mapForecastByTime(pointsWithCorrectSources);
+  private enrichedBeachData(
+    points: ForecastPoint[],
+    beach: Beach
+  ): BeachForecast[] {
+    return points.map((point) => ({
+      ...{
+        lat: beach.lat,
+        lng: beach.lng,
+        name: beach.name,
+        position: beach.position,
+        rating: 1,
+      },
+      ...point,
+    }));
   }
 
   private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
