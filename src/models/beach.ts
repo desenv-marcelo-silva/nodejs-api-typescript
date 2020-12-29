5d372a2c9d3195c3fd60a56902500b224dcdce74
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-29 17:37:24 -0300

    feat: create mongoose model Beach
    Remove id from interface Beach

---
+++ src/models/beach.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+import { Beach as IBeach } from '@src/services/Beach';
+
+export interface Beach extends IBeach {
+  _id?: string;
+}
+
+const schema = new mongoose.Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+interface BeachModel extends Omit<Beach, '_id'>, Document {}
+
+export const Beach: Model<BeachModel> = mongoose.model('Beach', schema);
--- src/services/Beach.ts
+++ src/services/Beach.ts
@@ -1,6 +1,6 @@
 import { BeachPosition } from './BeachPosition';
-
 export interface Beach {
+  _id?: string;
   name: string;
   position: BeachPosition;
   lat: number;
