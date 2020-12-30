2f5a08290f0332485685d602040cfc8afb6e7bf9
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-30 18:41:15 -0300

    feat: use middleware to control access to create and filter beaches

--- src/models/beach.ts
+++ src/models/beach.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from 'mongoose';
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 import { Beach as IBeach } from '@src/services/Beach';
 
@@ -12,6 +12,7 @@ const schema = new mongoose.Schema(
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
