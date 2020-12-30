686052383245718cd2cd3ab149b6603f240e07d1
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-12-30 12:42:05 -0300

    feat(users): create a custom validator on model

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,6 +7,10 @@ export interface User {
   password: string;
 }
 
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
+}
+
 interface UserModel extends Omit<User, '_id'>, Document {}
 
 const schema = new mongoose.Schema(
@@ -15,7 +19,6 @@ const schema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: [true, 'Your e-mail must be unique'],
     },
     password: { type: String, required: true },
   },
@@ -28,6 +31,15 @@ const schema = new mongoose.Schema(
       },
     },
   }
+);
+
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
