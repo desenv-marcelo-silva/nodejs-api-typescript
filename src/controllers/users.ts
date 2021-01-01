a70644129becca40dfe26fcb01c3fc214a8d784e
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2021-01-01 13:23:57 -0300

    feat: 'me' route to return user info

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 
 import AuthService from '@src/services/auth';
 import { User } from '@src/models/user';
 import { BaseController } from '@src/controllers/index';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -42,5 +43,19 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return res.send({ ...user.toJSON(), ...{ token } });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+    return res.send({ user });
   }
 }
